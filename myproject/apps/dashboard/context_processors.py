f9e83e978f8f96ba4705f10372b9e75c142a6840
Jhonnikek <138178283+Jhonnikek@users.noreply.github.com>
2026-07-03 12:46:18 -0500

    refactor: move project from myproject/ to repo root

--- myproject/apps/dashboard/context_processors.py
+++
@@ -1,9 +0,0 @@
-from catalog.models import Product
-
-
-def dashboard_sidebar(request):
-    if request.user.is_authenticated and request.user.is_staff:
-        return {
-            "low_stock_count": Product.objects.filter(stock__lte=3).count()
-        }
-    return {}
